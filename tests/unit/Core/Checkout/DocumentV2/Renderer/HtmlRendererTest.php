3d6fcfb9e974796c7244adb29c6dfb3b6f9a1a57
Lars Kemper <l.kemper@shopware.com>
2026-05-19 15:02:02 +0200

    fix: resolve thread

--- tests/unit/Core/Checkout/DocumentV2/Renderer/HtmlRendererTest.php
+++ tests/unit/Core/Checkout/DocumentV2/Renderer/HtmlRendererTest.php
@@ -109,7 +109,7 @@ class HtmlRendererTest extends TestCase
         static::assertSame('invoice_12345', $result->fileName);
     }
 
-    public function testRenderToStringPrintModeUsesPdfFileTypeAndConfiguredItemsPerPage(): void
+    public function testRenderToStringPrintModeUsesConfiguredItemsPerPage(): void
     {
         $renderData = $this->createRenderData();
 
@@ -118,6 +118,8 @@ class HtmlRendererTest extends TestCase
             ->method('find')
             ->with(self::HTML_TEMPLATE_PATH)
             ->willReturn(self::HTML_TEMPLATE_PATH);
+
+        $rendered = '<html>print</html>';
 
         $env = $this->createMock(Environment::class);
         $env->expects($this->once())
@@ -132,7 +134,7 @@ class HtmlRendererTest extends TestCase
                     return true;
                 })
             )
-            ->willReturn('<html>print</html>');
+            ->willReturn($rendered);
 
         $input = new RenderInput(
             DocumentType::INVOICE->value,
@@ -141,11 +143,17 @@ class HtmlRendererTest extends TestCase
             [InvoiceDataProvider::KEY => $renderData],
         );
 
-        $this->createRenderer($finder, $env)->renderToString(
+        $result = $this->createRenderer($finder, $env)->renderToString(
             $input,
             new RenderState(),
             Context::createDefaultContext(),
         );
+
+        static::assertSame(DocumentFormat::HTML->value, $result->format);
+        static::assertSame($rendered, $result->content);
+        static::assertSame('html', $result->fileExtension);
+        static::assertSame('text/html', $result->mimeType);
+        static::assertSame('12345', $result->fileName);
     }
 
     public function testShouldThrowIfRenderDataCantBeFound(): void
